9ceef1136bfd5df632ece6bf62c9b553cc47ad90
“l.sommarberg” <laura.sommarberg@gmail.comgit config --global user.email laura.sommarberg@gmail.com>
2023-11-06 14:25:05 +0200

    varasto tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,37 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ei_negatiivista_lisaysta(self):
+        initial_saldo = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-5)
+        self.assertAlmostEqual(self.varasto.saldo, initial_saldo)
+
+    def test_ei_negatiivista_ottoa(self):
+        self.varasto.lisaa_varastoon(8)
+        saatu_maara = self.varasto.ota_varastosta(-2)
+        self.assertAlmostEqual(saatu_maara, 0.0)
+
+    def test_varasto_ei_negatiivinen(self):
+        self.varasto.lisaa_varastoon(8)
+        saatu_maara = self.varasto.ota_varastosta(10)
+        self.assertAlmostEqual(saatu_maara, 8)
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+
+    def test_lisays_ei_mene_yli(self):
+        self.varasto.lisaa_varastoon(10)
+        self.varasto.lisaa_varastoon(15)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+    
+    def test_ei_negatiivista_tilavuutta(self):
+        self.varasto = Varasto(-10)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_ei_negatiivista_alkusaldoa(self):
+        varasto = Varasto(10, alku_saldo=-5)
+        self.assertAlmostEqual(varasto.saldo, 0.0)
+
+    def test_str_tulostus(self):
+        self.varasto.lisaa_varastoon(5)
+        haluttu_tulos = "saldo = 5, vielä tilaa 5"
+        self.assertEqual(str(self.varasto), haluttu_tulos)
